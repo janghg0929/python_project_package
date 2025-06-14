15cd966f0a553b64cf5ee1320c3890117fc016c8
j_mm <janghangyul0929@gmail.com>
2025-06-14 12:47:44 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,66 @@
-from user import UserManager
-import os
-import time
+class MenuItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
-os.system("cls")
+Item1 = MenuItem("아메리카노", 3000)
+Item2 = MenuItem("라떼", 4000)
+Item3 = MenuItem("케이크", 5000)
 
-def main():
-    manager = UserManager()
+orderitem = []
+orderprice = []
+totalprice = []
 
-    while True:
+class Order:
+    def __init__(self, items):
+        self.items = items
+        Order.add_item(self.items)
 
-        try:
-            answer = int(input("유저 추가: 1\n목록 보기: 2\n종료: 3\n: "))
+    def add_item(menu_item):
+        if menu_item.name == Item1.name:
+            if orderitem.count(Item1.name) < 1:
+                orderitem.append(menu_item.name)
+        elif menu_item.name == Item2.name:
+            if orderitem.count(Item2.name) < 1:
+                orderitem.append(menu_item.name)
+        elif menu_item.name == Item3.name:
+            if orderitem.count(Item3.name) < 1:
+                orderitem.append(menu_item.name)
+        orderprice.append(menu_item.name)
+        totalprice.append(menu_item.price)
 
-            os.system("cls")
+    def total_price():
+        print(f"총 결제 금액: {sum(totalprice)}원")
 
-            if answer == 1:
-                manager.add_user(input("유저의 이름을 입력해 주세요\n: "), input("유저의 이메일을 입력해 주세요\n: "))
-                print("유저가 성공적으로 추가 되었습니다!")
-                time.sleep(3)
+    def show_order():
+        print("\n=== 주문 내역 ===")
+        for i in range(len(orderitem)):
+            print(f"{orderitem[i]} - {orderprice.count(orderitem[i]) * Item1.price}원" if orderitem[i] == Item1.name else f"{orderitem[i]} - {orderprice.count(orderitem[i]) * Item2.price}원" if orderitem[i] == Item2.name else f"{orderitem[i]} - {orderprice.count(orderitem[i]) * Item3.price}원")
+        Order.total_price()
 
-            elif answer == 2:
-                print("이름 - 이메일\n" + "-" * 13)
-                for user in manager.list_users():
-                    print(f"{user['username']} - {user['email']}")
-                input("\n나가기 위해 아무 단어나 입력\n: ")
+class main:
+    def main():
 
-            elif answer == 3:
-                break
+        print("===== 메뉴 =====")
+        print(f"1. {Item1.name} - {Item1.price}원")
+        print(f"2. {Item2.name} - {Item2.price}원")
+        print(f"3. {Item3.name} - {Item3.price}원")
+        print("0. 주문 완료")
+        print("=================")
 
-            else:
-                os.system("cls")
-                print("1, 2, 3 중에서 선택해 주세요\n")
-                continue
+        while True:
+            jumun = int(input("메뉴 선택: "))
+            if jumun == 1:
+                Order(Item1)
+                print(f"{Item1.name} 추가되었습니다.")
+            elif jumun == 2:
+                Order(Item2)
+                print(f"{Item2.name} 추가되었습니다.")
+            elif jumun == 3:
+                Order(Item3)
+                print(f"{Item3.name} 추가되었습니다.")
+            elif jumun == 0:
+                Order.show_order()
+                return
 
-            os.system("cls")
-
-        except:
-            os.system("cls")
-            print("1, 2, 3 중에서 선택해 주세요\n")
-            continue
-
-if __name__ == "__main__":
-    main()
-print("종료되었습니다")
+main.main()
